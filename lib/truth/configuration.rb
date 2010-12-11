1bf8a5f3d6e131afa9a24e41929b16e27a95013f
Jay Adkisson <j4yferd@gmail.com>
2010-12-11 11:49:31 -0800

    clean up configuration.rb
    - remove an unused method
    - call the initializer correctly
    - no explicit indices

--- lib/truth/configuration.rb
+++ lib/truth/configuration.rb
@@ -7,23 +7,14 @@ module Truth
 
       def version(ver)
         versions.get(ver.to_i) do
-          new(:version => ver.to_i)
+          new(ver.to_i)
         end
-      end
-
-      def objects(klass)
-        @objects ||= {}
-        @objects[klass.name] ||= Index.new
       end
     end
 
     attr_reader :version
     def initialize(version)
       @version = version
-    end
-
-    index :host, :plural => :hosts do |name|
-      Host.new(self, name.to_sym)
     end
 
     # end the tree-climbing here.
